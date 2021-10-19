ef13afa4646071502d828780cf020a2c388150ee
heromantor <phpmorphy@ya.ru>
2021-10-19 16:29:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,14 +3,6 @@ importScripts('/app.js');
 
 messaging = hmsMessaging();
 
-// messaging.setBackgroundMessageHandler(function (payload) {
-//     console.log('[hms-messaging-sw.js] Received background message.', payload);
-//     // Customize the notification panel.
-//     const notificationTitle = 'Background Message Title';
-//     const notificationOptions = {
-//         body: 'Background Message body.',
-//         icon: '/hms-logo.png'
-//     };
-
-//      return self.registration.showNotification(notificationTitle, notificationOptions);
-// });
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[hms-messaging-sw.js] Received background message.', payload);
+});
